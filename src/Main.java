e520a34fba8b826304c5b14e930f280ddc501221
Nice <nicole.scheffel@bluemail.ch>
2017-01-26 13:06:16 +0100

    main class added

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,64 @@
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.LineChart;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
 /**
- * @author Lorenz Rasch
+ * @author Lorenz Rasch & Nicole Scheffel
  */
-public class Main {
+
+public class Main extends Application {
+
+	@Override public void start(Stage stage) throws Exception{
+		
+		stage.setTitle("Weather Forecast");
+		
+    	BorderPane root = new BorderPane();
+    	root.setBottom(new Label("Created by Lorenz Rasch & Nicole Scheffel"));
+    	
+    	Forecast f = OWMReader.readOWMFile(getClass().getResource("/forecast.xml").getFile(), "Bern");
+    	
+    	//Center
+    	FlowPane flowPane = new FlowPane();
+    	root.setCenter(flowPane);
+    	
+    	BarChart<String, Number> barChart = ChartLoader.loadPercipitationChart(f);
+    	double height = barChart.prefHeight(flowPane.getMaxWidth()*0.5);
+    	barChart.resize(flowPane.getMaxWidth()*0.5, height);
+    	flowPane.getChildren().add(barChart);
+    	
+    	LineChart<String, Number> lineChart = ChartLoader.loadTemperatureChart(f);
+    	flowPane.getChildren().add(lineChart);
+    	
+    	//Top
+    	HBox hboxTop = new HBox();
+    	root.setTop(hboxTop);
+        hboxTop.setAlignment(Pos.CENTER);
+    	
+    	ChoiceBox<String> choiceBox = new ChoiceBox<String>(FXCollections.observableArrayList(
+    			"Bern", "Biel", "Basel", "Zuerich", "Genf")
+    	);
+        //Set a default value
+        choiceBox.setValue("Bern");
+        
+        hboxTop.getChildren().add(choiceBox);
+
+        Scene scene  = new Scene(root,1000,700);
+  
+        stage.setScene(scene);
+        stage.show();
+	}
 
     public static void main(String[] args) {
-
-
-        Forecast f;
-        
+        launch(args);
     }
 
 }
