69998dead621ad189ed2f780193893d1723cb2dd
Nice <nicole.scheffel@bluemail.ch>
2017-01-28 11:35:23 +0100

    Added icon depending on id

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,22 @@
 import javafx.application.Application;
 import javafx.collections.FXCollections;
-import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.LineChart;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollBar;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.util.Arrays;
-import java.util.EnumSet;
 import java.util.List;
 
 /**
@@ -34,7 +34,7 @@ public class Main extends Application {
 		
     	BorderPane root = new BorderPane();
 
-        Scene scene  = new Scene(root,1000,600);
+        Scene scene  = new Scene(root,1000,700);
     	
     	//Bottom
     	root.setBottom(new Label("Created by Lorenz Rasch & Nicole Scheffel"));
@@ -53,32 +53,46 @@ public class Main extends Application {
     	
     	//BarChart Center
     	BarChart<String, Number> barChart = ChartLoader.loadPercipitationChart(f);
-    	double height = barChart.prefHeight(flowPane.getMaxWidth()*0.5);
-    	barChart.resize(flowPane.getMaxWidth()*0.5, height);
-    	flowPane.getChildren().add(barChart);
+    	barChart.setPrefSize(500, 300);
+    	barChart.setMinSize(400, 200);
+    	barChart.setMaxSize(600, 400);
     	
     	//LineChart Center
     	LineChart<String, Number> lineChart = ChartLoader.loadTemperatureChart(f);
-    	flowPane.getChildren().add(lineChart);
+    	lineChart.setPrefSize(500, 300);
+    	lineChart.setMinSize(400, 200);
+    	lineChart.setMaxSize(600, 400);
     	
     	//TableView Center
     	final TableView<String> table = new TableView<String>();
     	table.setEditable(false);
-    	 
+    	table.setPrefSize(400, 300);
+    	table.setMinSize(200, 200);
+    	table.setMaxSize(400, 400);
+    	
         TableColumn<String, String> description = new TableColumn<String, String>("Parameter");
         TableColumn<String, String> number = new TableColumn<String, String>("Amount/Number");
+        //Change width
         description.prefWidthProperty().bind(table.widthProperty().divide(2));
         number.prefWidthProperty().bind(table.widthProperty().divide(2));
          
         table.getColumns().addAll(description, number);
     	
-        flowPane.getChildren().add(table);
+        //Icon
+        Image image = new Image ("http://openweathermap.org/img/w/" + f.getWeather().get(0).getId() + ".png");
+        ImageView img = new ImageView();
+        img.setImage(image);
+        img.setPreserveRatio(true);
+        img.setFitWidth(100);
+        
+        //Add everything to the flowPane in the center
+        flowPane.getChildren().addAll(barChart, table, lineChart, img);
 
     	//Top
     	HBox hboxTop = new HBox();
     	root.setTop(hboxTop);
         hboxTop.setAlignment(Pos.CENTER);
-
+        
 		ChoiceBox<OWMCity> choiceBox = new ChoiceBox<OWMCity>(FXCollections.observableArrayList(cityList));
         //Set a default value
         choiceBox.setValue(OWMCity.BERN);
